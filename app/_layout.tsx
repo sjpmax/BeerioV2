3493d97ddbe11de465a2c3834b0cb201ebf5ddcb
Jonny P <jonnymadethis@gmail.com>
2025-10-12 11:39:40 -0400

    removed example app and updated layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,53 @@
 
-import "../global.css";
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { PaperProvider, MD3DarkTheme } from 'react-native-paper';
+import { phillyColors } from '@/constants/colors';
+
+export const unstable_settings = {
+    anchor: '(tabs)',
+};
+
+//'navy': '#001F3F',      // Deep navy blue background
+//'midnight': '#0A1929',   // Even darker blue for cards/contrast
+//'gold': '#D4AF37',       // Classic gold (good contrast)
+//'dark-gold': '#B8860B',  // Dark goldenrod for subtle text
+//'accent': '#FFB302',     // Bright gold for highlights/buttons
+//'unselected': '#DB7F40', // Muted orange for unselected items
+
+const Theme = {
+    ...MD3DarkTheme,
+    colors: {
+        ...MD3DarkTheme.colors,
+        primary: phillyColors.accent,
+        secondary: phillyColors.gold,
+        background: phillyColors.navy,
+        surface: phillyColors.midnight,
+        surfaceVariant: phillyColors.midnight,
+        onSurface: phillyColors.gold,
+        onSurfaceVariant: phillyColors.darkGold,  // dark gold
+    },
+};
 
 export default function RootLayout() {
-    return <Stack screenOptions={{ headerShown: false }} />;
+
+    return (
+        <PaperProvider theme={Theme} >
+            <Stack
+                screenOptions={{
+                    headerStyle: {
+                        backgroundColor: '#f4511e',
+                    },
+                    headerTintColor: '#fff',
+                    headerTitleStyle: {
+                        fontWeight: 'bold',
+                    },
+                }}>
+                <Stack.Screen name="(tabs)" options={{ headerShown: true }} />
+                <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+            </Stack>
+            <StatusBar style="auto" />
+        </PaperProvider>
+
+    );
 }
