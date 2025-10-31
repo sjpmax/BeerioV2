a2a790f7a6d140c036718204259f01149d42cede
Jonny P <jonnymadethis@gmail.com>
2025-10-31 15:40:05 -0400

    maps and everything else working

--- components/_beer_map_view.tsx
+++ components/_beer_map_view.tsx
@@ -4,6 +4,7 @@ import { Theme } from '@react-navigation/native';
 import * as Location from 'expo-location';
 import React from 'react';
 import { View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 
 interface BeerSuggestionProps {
@@ -21,8 +22,33 @@ export default function BeerMapView({
     locationStatus,
     getDistanceMessage
 }: BeerSuggestionProps) {
+console.log('Rendering BeerMapView with beers:', groupedBeers);
+
     return (
         <View style={{ flex: 1, backgroundColor: theme.colors.background }}>
+            <MapView
+                style={{ flex: 1 }}
+                initialRegion={{
+                    latitude: location?.coords.latitude || 0,
+                    longitude: location?.coords.longitude || 0,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+            >
+                {Object.values(groupedBeers).map((beerGroup, index) => (
+                    beerGroup.locations.map((loc) => (
+                        <Marker
+                            key={index}
+                            coordinate={{
+                                latitude: loc.bar_lat,
+                                longitude: loc.bar_long,
+                            }}
+                            title={beerGroup.name}
+                            description={`${loc.bar_name} - ${getDistanceMessage(loc.bar_lat, loc.bar_long)}`}  
+                        />
+                    ))
+                ))}
+            </MapView>
             
         </View>
     );
